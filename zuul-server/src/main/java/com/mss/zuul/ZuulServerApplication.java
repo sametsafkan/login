57b5193ca245b7388d35082a98ebccde8ae88fe5
sametsafkan <sametsafkan@192.168.1.26>
2017-11-02 22:20:03 +0300

    zuul gateway

---
+++ zuul-server/src/main/java/com/mss/zuul/ZuulServerApplication.java
@@ -0,0 +1,20 @@
+package com.mss.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+/**
+ * 
+ * @author sametsafkan
+ *
+ */
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+}
