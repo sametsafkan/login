5ad35094e2898c9a99e1dd04e9e1aa0a5bf39ed3
sametsafkan <sametsafkan@192.168.1.26>
2017-11-21 20:54:43 +0300

    zull proxy post filter.

---
+++ zuul-server/src/main/java/com/mss/zuul/PostFilter.java
@@ -0,0 +1,52 @@
+package com.mss.zuul;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+/**
+ * 
+ * @author sametsafkan
+ *
+ */
+public class PostFilter extends ZuulFilter{
+	
+	private static Logger log = LoggerFactory.getLogger(PostFilter.class);
+
+	@Autowired
+	TokenServiceClient tokenService;
+
+	public PostFilter(TokenServiceClient tokenService) {
+		this.tokenService = tokenService;
+	}
+	
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		String token = tokenService.generateToken();
+		ctx.getResponse().addHeader("token", token);
+		log.debug("Zuul Proxy Post Filter token : " + token);
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String filterType() {
+		return "post";
+	}
+
+}
